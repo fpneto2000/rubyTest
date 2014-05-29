cfa072dc18cedfc007acdff16eda3b64bb42d04e
Florencio Pereira Neto <florenciopereiraneto@MacBook-de-Florencio-Pereira-Neto.local>
2014-05-28 23:18:05 -0300

    finalização do login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authorize
   def new
   end
 
   def create
     if usuario = Usuario.authenticate(params[:login], params[:password])
       session[:usuario_id] = usuario.id
+      session[:usuario_nome] = usuario.nome
       redirect_to admin_url
     else
       redirect_to login_url, :alert => "Usuario e/ou Senha invalidos"
