dba7e9bbad1a54662b528406ad7d88b3857b405f
Florencio Pereira Neto <florenciopereiraneto@MacBook-de-Florencio-Pereira-Neto.local>
2014-06-16 21:21:15 -0300

    layout de consulta de participantes

--- app/models/serienum.rb
+++ app/models/serienum.rb
@@ -2,5 +2,6 @@ class Serienum < ActiveRecord::Base
   belongs_to :produto
   belongs_to :fornecedor, :class_name=>'Participante', foreign_key: 'fornecedor_id'
   belongs_to :fabricante, :class_name=>'Participante', foreign_key: 'fabricante_id'
-
+  
+  validates :numero,:presence => { message: " - Campo obrigatorio"}
 end
